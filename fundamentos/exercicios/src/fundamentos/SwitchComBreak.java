81cf6c3d0cb862697ec0caad495ccc32bf6c826e
LuisFelipeMTCS <luisfelipetoledo.profissional@gmail.com>
2023-01-26 23:38:57 -0300

    INCLUSAO DO SCANNER

--- fundamentos/exercicios/src/fundamentos/SwitchComBreak.java
+++ fundamentos/exercicios/src/fundamentos/SwitchComBreak.java
@@ -1,23 +1,28 @@
 package exercicios.src.fundamentos;
 
+import java.util.Scanner;
+
 public class SwitchComBreak {
     public static void main(String[] args) {
-        String branco = "azul";
-        switch(branco.toLowerCase()){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite uma cor e tente a sorte");
+        String valor = scan.nextLine();   
+        switch(valor.toLowerCase()){
             case "branco":
                 System.out.println("Cor Branca");
                 break;
             case "azul":case "rosa":
-                System.out.println("Cor azul e rosa");
+                System.out.println("Cor azul ou rosa");
                 break;
             
             case "amarelo":case "verde":
-                System.out.println("Cor amarela e Verde");
+                System.out.println("Cor amarela ou Verde");
                 break;
             
-
             default:
                 System.out.println("Nenhuma das cores mencionada");
+            scan.close();
+
                 
         System.out.println("FIM");
 
